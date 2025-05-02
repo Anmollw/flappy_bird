3597897626871e3919a798e297484032c04f243f
Anmol Wadhwa <anmolwadhwa2006@outlook.com>
2025-05-02 20:01:22 +0530

    added Pipe Spawn Script

---
+++ PipeSpawnScript.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class PipeSpawnScript : MonoBehaviour
+{
+     public GameObject Pipe ; 
+     public float Spawnrate = 2;
+     private float timer = 0 ;
+     public float heightoffset = 10 ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnpipe() ;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer < Spawnrate) 
+        {
+            timer= timer + Time.deltaTime ; 
+        }
+        else
+        {
+            spawnpipe() ;  
+
+            timer = 0 ;
+        }
+    }
+    void spawnpipe() 
+    {
+        float lowestpoint = transform.position.y - heightoffset ;
+        float highestpoint = transform.position.y + heightoffset ;
+        Instantiate( Pipe , new Vector3(transform.position.x , Random.Range(lowestpoint , highestpoint) ,0) , transform.rotation) ;
+    }
+
+}
